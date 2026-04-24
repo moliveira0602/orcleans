fda11ccae522cfc4569fd7ec695043a94c1a9dcf
Marcos Oliveira <mherculano.oliveira@gmail.com>
2026-04-24 01:45:19 +0100

    debug: direct ping in api entry point

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,3 +1,7 @@
 import { app } from '../src/index';
 
+(app as any).get('/ping-direct', (req: any, res: any) => {
+  res.send('ping direct v4 - ' + new Date().toISOString());
+});
+
 export default app;
